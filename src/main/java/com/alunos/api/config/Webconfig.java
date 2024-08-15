72c5e2b33b68f5a5ccd309623cbcaa3928386be7
cauefb <ferreira.caue07@gmail.com>
2024-08-15 12:24:46 -0300

    ajuste cors

--- src/main/java/com/alunos/api/config/Webconfig.java
+++ src/main/java/com/alunos/api/config/Webconfig.java
@@ -1,14 +1,19 @@
 package com.alunos.api.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-@EnableWebMvc
-public class Webconfig {
+public class Webconfig implements WebMvcConfigurer {
 
+    @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**");
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("*")
+                .allowCredentials(true);
     }
 }
