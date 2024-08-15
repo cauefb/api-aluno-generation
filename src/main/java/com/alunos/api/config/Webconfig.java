9d2f665f6e43d86d24f38a74703c4c8848943af6
cauefb <ferreira.caue07@gmail.com>
2024-08-15 12:34:30 -0300

    url cors

--- src/main/java/com/alunos/api/config/Webconfig.java
+++ src/main/java/com/alunos/api/config/Webconfig.java
@@ -10,7 +10,7 @@ public class Webconfig {
 
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("*")
+                .allowedOrigins("http://api-aluno-generation-production.up.railway.app/v1/students")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
