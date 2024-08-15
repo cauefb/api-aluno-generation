d44d0106eff54a74c89a1b2ebd0b0d1e97867a89
cauefb <ferreira.caue07@gmail.com>
2024-08-15 12:55:44 -0300

    webconfig

--- src/main/java/com/alunos/api/config/Webconfig.java
+++ src/main/java/com/alunos/api/config/Webconfig.java
@@ -1,18 +1,23 @@
 package com.alunos.api.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import jakarta.validation.OverridesAttribute;
 
 @Configuration
 @EnableWebMvc
 public class Webconfig {
 
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")
-                .allowedOrigins("http://api-aluno-generation-production.up.railway.app/v1/students")
-                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-                .allowedHeaders("*")
-                .allowCredentials(true);
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**");
+            }
+        };
     }
 }
