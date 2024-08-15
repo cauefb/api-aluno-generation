ac59ccf4d7e372e5ed4939c5d41c6c4fc4dbdb3a
cauefb <ferreira.caue07@gmail.com>
2024-08-15 12:28:15 -0300

    ajuste cors

--- src/main/java/com/alunos/api/config/Webconfig.java
+++ src/main/java/com/alunos/api/config/Webconfig.java
@@ -11,7 +11,7 @@ public class Webconfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("*")
+                // .allowedOrigins("*")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
